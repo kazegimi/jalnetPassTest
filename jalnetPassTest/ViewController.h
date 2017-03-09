a926022d511b901e25bc66ee2444a144a153cf95
光田征史 <semicchann@micchann-macbook-pro.local>
2017-03-09 16:08:10 +0900

    Initial Commit

---
+++ jalnetPassTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jalnetPassTest
+//
+//  Created by Seiji Mitsuda on 2017/03/09.
+//  Copyright © 2017年 Another Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jalnetPassTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  jalnetPassTest
+//
+//  Created by Seiji Mitsuda on 2017/03/09.
+//  Copyright © 2017年 Another Sky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
